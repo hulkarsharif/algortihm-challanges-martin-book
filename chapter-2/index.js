ff1a67d147b8411efd7120657672e55442b55ac5
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-10-02 10:15:51 -0400

    Finish the chapter-3

--- chapter-2/index.js
+++ chapter-2/index.js
@@ -52,5 +52,153 @@ const messyMath = (num) => {
 };
 console.log(messyMath([4], 6));
 
+console.log("----------------------");
+
 //Fibonacci
-const fibonacci = (size) => {};
+const fibonacci = (size) => {
+    let sum = [0, 1];
+    for (let i = 2; i <= size; i++) {
+        sum[i] = sum[i - 1] + sum[i - 2];
+    }
+    return sum;
+};
+console.log(fibonacci(10));
+
+//isPrime
+
+const isPrime = (num) => {
+    if (num < 1) {
+        return false;
+    }
+    for (i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isPrime(7));
+console.log("-------------------");
+
+// Sweat Shirt Pricing
+const sweatShirtPricing = (num) => {
+    let discount = 0;
+    if (num === 2) {
+        discount = 0.09;
+    } else if (num === 3) {
+        discount = 0.19;
+    } else if (num >= 4) {
+        discount = 0.35;
+    }
+    return Math.ceil(num * 20 - num * 20 * discount);
+};
+console.log(sweatShirtPricing(10));
+
+// Extract-o-matic
+
+const extractDigit = (num, digitNum) => {
+    num = Math.abs(num);
+    let nthDigit = digitNum >= 0 ? 10 ** digitNum : 10 ** Math.abs(digitNum);
+
+    if (digitNum >= 0) {
+        return Math.floor(num / nthDigit) % 10;
+    }
+
+    return Math.floor(num * nthDigit) % 10;
+};
+console.log(extractDigit(34567, 1));
+
+// Most significant digit. Leftmost non-zero digit
+
+const mostSignificantDigit = (number) => {
+    number = Math.abs(number);
+    if (number > 1) {
+        while (number >= 10) {
+            number = number / 10;
+        }
+    } else {
+        while (number < 1) {
+            number * 10;
+        }
+    }
+    return Math.floor(number);
+};
+console.log(mostSignificantDigit(12345));
+
+// Gaming Fundamentals
+const rollOne = () => {
+    return Math.ceil(Math.random() * 6);
+};
+
+const playFives = (num) => {
+    for (let i = 0; i < num; i++) {
+        let result = rollOne();
+        console.log(result);
+        if (result === 5) {
+            console.log("Thats good luck");
+        }
+    }
+};
+const playStatistics = (num) => {
+    let min = null;
+    let max = null;
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+        let result = rollOne();
+        sum += result;
+        if (min === null) {
+            min = result;
+        }
+        if (max === null) {
+            max = result;
+        }
+
+        if (min > result) min = result;
+        if (max < result) max = result;
+    }
+
+    return {
+        min,
+        max,
+        sum,
+        average: sum / num
+    };
+};
+
+// Statistics Untill Doubles
+
+const rollOne20 = () => {
+    return Math.ceil(Math.random() * 20);
+};
+
+const statisticsUntilDoubles = () => {
+    let current = rollOne20();
+    let next = rollOne20();
+    let rolls = 2;
+    let sum = current + next;
+    let min = current;
+    let max = next;
+
+    while (current !== next) {
+        let result = rollOne20();
+        rolls++;
+        sum += result;
+        if (min > result) min = result;
+        if (max < result) max = result;
+        current = next;
+        next = result;
+    }
+
+    return {
+        min,
+        max,
+        rolls,
+        sum,
+        average: sum / rolls,
+        current,
+        next
+    };
+};
+
+console.log(statisticsUntilDoubles());
