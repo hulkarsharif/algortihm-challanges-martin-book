bf5772fb25a7399bcc961f41a1eb021c5382df81
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-10-20 11:17:48 -0400

    Finish the HTML valid assignment

--- chapter-4/string.js
+++ chapter-4/string.js
@@ -8,3 +8,36 @@ const getDigits1 = (string) => {
     return result.join("");
 };
 console.log(getDigits1("0s1a3y5w7h9a2t4?6!8?0"));
+
+//Input: Array of HTML tags. Identify if it has a valid structure
+//['<header>',</header>,<div,</div>]
+//[<header>,<div>,</headrer>,</div>]
+
+const isOpeningTag = (tag) => {
+    return tag[1] !== "/";
+};
+
+const convertToOpeningTag = (closingTag) => {
+    return closingTag[0] + closingTag.slice(2);
+};
+const isHtmlValid = (tags) => {
+    const holder = [];
+
+    for (const tag of tags) {
+        if (isOpeningTag(tag)) {
+            holder.push(tag);
+        } else {
+            const lastTag = holder.pop();
+            const openingTag = convertToOpeningTag(tag);
+
+            if (lastTag !== openingTag) {
+                return false;
+            }
+        }
+    }
+    return holder.length === 0;
+};
+
+// console.log(isHtmlValid(["<div>"]));
+// console.log(isHtmlValid(["<div>", "</div>"]));
+// console.log(isHtmlValid(["</div>", "<div1>"]));
