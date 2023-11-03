6bc507c7f617ffb7c258bf4ab6a44b46595bc4ab
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-11-03 17:36:45 -0400

    Finish the class assessment

--- chapter-4/string.js
+++ chapter-4/string.js
@@ -149,3 +149,83 @@ const romanNum = (nums) => {
 // Example usage:
 console.log(romanNum(349)); // Outputs "CCCIL"
 console.log(romanNum(444)); // Outputs "CDXLIV"/not yet resulted
+
+const isPalindrome = (str) => {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+};
+console.log(isPalindrome("rotetor"));
+console.log(isPalindrome("hello"));
+
+const isPalindrome = (str) => {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+};
+console.log(isPalindrome("rotetor"));
+console.log(isPalindrome("hello"));
+
+const fizzBuzz = (numbers) => {
+    for (let i = 0; i < numbers; i++) {
+        if (i % 15 === 0) {
+            console.log("FizzBUzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        }
+    }
+};
+
+//Construct a function that counts the number of vowels in a string.
+const countVowels = (str) => {
+    let count = 0;
+    let vowels = "aeiou";
+    for (const char of str) {
+        if (vowels.includes(char)) count++;
+    }
+    return { vovels: count, consonantnts: str.length - count };
+};
+console.log(countVowels("apple"));
+
+//Develop a function that determines if two strings are anagrams of each other. Return true if anagrams, else false.
+// e.g ("secure", "rescue")
+// secure   =>  rescue
+//HELLO, ELLOH\
+// APPLE, LEAPP
+// PULLS, PULLR
+const isAnagram = (str1, str2) => {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    str1Count = {};
+    str2Count = {};
+    for (const char of str1) {
+        if (str1Count[char]) {
+            str1Count[char]++;
+        } else {
+            str1Count[char] = 1;
+        }
+    }
+    for (const char of str2) {
+        if (str2Count[char]) {
+            str2Count[char]++;
+        } else {
+            str2Count[char] = 1;
+        }
+    }
+    for (const char in str1Count) {
+        if (str1Count[char] !== str2Count[char]) {
+            return false;
+        }
+    }
+    return true;
+};
+console.log(isAnagram("secure", "rescue"));
